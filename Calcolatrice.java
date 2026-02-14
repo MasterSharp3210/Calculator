4d68104644346b7c04ef311227ba0a94e3eb8d3d
MasterSharp <marco.delpretehome25@outlook.com>
2026-02-14 20:55:01 +0100

    Add files via upload

--- Calcolatrice.java
+++ Calcolatrice.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.awt.*; 
 import java.awt.event.*;
 import java.io.*;
 import javax.swing.*;
@@ -24,11 +24,11 @@ public class Calcolatrice extends JFrame implements ActionListener {
         JPanel panel = new JPanel(new GridLayout(5, 4, 5, 5));
 
         String[] tasti = {
-            "√", "x²", "/", "*",
+            "√", "x²", "÷", "X",
             "7", "8", "9", "-",
             "4", "5", "6", "+",
             "1", "2", "3", ".",
-            "C", "0", "=", "#" 
+            "C", "0", "#", "=" 
         };
 
         for (String t : tasti) {
@@ -39,7 +39,78 @@ public class Calcolatrice extends JFrame implements ActionListener {
         }
 
         add(panel, BorderLayout.CENTER);
+
+        // --- KEY BINDINGS PROFESSIONALI ---
+        setupKeyBindings();
+
         setVisible(true);
+    }
+
+    private void setupKeyBindings() {
+        JComponent comp = this.getRootPane();
+        InputMap im = comp.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap am = comp.getActionMap();
+
+        // Numeri 0-9 (tastiera normale e tastierino)
+        for (int i = 0; i <= 9; i++) {
+            final String num = String.valueOf(i);
+
+            im.put(KeyStroke.getKeyStroke(num), num);
+            im.put(KeyStroke.getKeyStroke("NUMPAD" + i), num);
+
+            am.put(num, new AbstractAction() {
+                public void actionPerformed(ActionEvent e) {
+                    display.setText(display.getText() + num);
+                }
+            });
+        }
+
+        // Punto
+        im.put(KeyStroke.getKeyStroke('.'), "dot");
+        im.put(KeyStroke.getKeyStroke("DECIMAL"), "dot");
+        am.put("dot", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                if (!display.getText().contains(".")) {
+                    display.setText(display.getText() + ".");
+                }
+            }
+        });
+
+        // Operatori
+        bindOperator("+");
+        bindOperator("-");
+        bindOperator("*");
+        bindOperator("/");
+
+        // Invio = risultato
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "equals");
+        am.put("equals", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                calcolaRisultato();
+            }
+        });
+    }
+
+    private void bindOperator(String op) {
+        JComponent comp = this.getRootPane();
+        InputMap im = comp.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap am = comp.getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(op), op);
+
+        am.put(op, new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                if (!display.getText().isEmpty()) {
+                    num1 = Double.parseDouble(display.getText());
+
+                    if (op.equals("*")) operatore = "X";
+                    else if (op.equals("/")) operatore = "÷";
+                    else operatore = op;
+
+                    display.setText("");
+                }
+            }
+        });
     }
 
     @Override
@@ -48,31 +119,14 @@ public class Calcolatrice extends JFrame implements ActionListener {
 
         if (comando.matches("[0-9]")) {
             display.setText(display.getText() + comando);
-        } else if (comando.matches("[+\\-*/]")) {
+        } else if (comando.matches("[+\\-*/X÷]")) {
             if (!display.getText().isEmpty()) {
                 num1 = Double.parseDouble(display.getText());
                 operatore = comando;
                 display.setText("");
             }
         } else if (comando.equals("=")) {
-            if (!operatore.isEmpty() && !display.getText().isEmpty()) {
-                double num2 = Double.parseDouble(display.getText());
-                double risultato = 0;
-
-                switch (operatore) {
-                    case "+": risultato = num1 + num2; break;
-                    case "-": risultato = num1 - num2; break;
-                    case "*": risultato = num1 * num2; break;
-                    case "/": risultato = num1 / num2; break;
-                }
-
-                display.setText(String.valueOf(risultato));
-                operatore = "";
-            } else if (!display.getText().isEmpty()) {
-                display.setText(display.getText());
-            } else {
-                display.setText("0");
-            }
+            calcolaRisultato();
         } else if (comando.equals("C")) {
             display.setText("");
             num1 = 0;
@@ -95,6 +149,23 @@ public class Calcolatrice extends JFrame implements ActionListener {
         } 
         else if (comando.equals("#")) {
             apriAppunti();
+        }
+    }
+
+    private void calcolaRisultato() {
+        if (!operatore.isEmpty() && !display.getText().isEmpty()) {
+            double num2 = Double.parseDouble(display.getText());
+            double risultato = 0;
+
+            switch (operatore) {
+                case "+": risultato = num1 + num2; break;
+                case "-": risultato = num1 - num2; break;
+                case "X": risultato = num1 * num2; break;
+                case "÷": risultato = num1 / num2; break;
+            }
+
+            display.setText(String.valueOf(risultato));
+            operatore = "";
         }
     }
 
